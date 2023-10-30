dbdc896b71bb84262302698cf581992a85972d84
deborah mizrahi <debimizfel@gmail.com>
2023-10-30 18:06:56 +0200

    .

--- 09-10_react-errors/auth_using-interceptors/src/Config.ts
+++ 09-10_react-errors/auth_using-interceptors/src/Config.ts
@@ -1,7 +1,7 @@
 class Config {
     public employeesUrl = "http://localhost:3030/api/employees/";
     public sigup = "http://localhost:3030/api/register/";
-    public login = "http://localhost:3030/api/login/";
+    public login = "";
     public categories = "http://localhost:3030/api/categories/";
 }
 
