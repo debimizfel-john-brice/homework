20d94d5e12899efb820f91334264e086ef8d1fba
deborah mizrahi <debimizfel@gmail.com>
2023-11-27 16:55:07 +0200

    .

---
+++ 11-08_auth_TODO/coffee-store/src/5-services/coffee_service.ts
@@ -0,0 +1,79 @@
+import { OkPacket } from "mysql";
+import CoffeeModel from "../2-models/coffe_model";
+import dal from "../4-utils/dal";
+import { IdNotFound } from "../2-models/error_status";
+
+async function getCoffees(): Promise<CoffeeModel[]> {
+    const sql = "SELECT * FROM coffees";
+    const cofees = await dal.execute(sql);
+    return cofees;
+}
+
+async function getCoffee(id: number): Promise<CoffeeModel> {
+    const sql = `SELECT * FROM coffees WHERE coffeeId = ${id}`;
+    const coffees = await dal.execute(sql);
+    const coffee = coffees[0];
+    if (!coffee) throw new IdNotFound(id);
+    return coffee;
+}
+
+async function getExpensiveCoffees(minPrice: number): Promise<CoffeeModel[]> {
+    const sql = `SELECT * FROM coffees WHERE price > ${minPrice}`;
+    const coffees = await dal.execute(sql);
+    return coffees;
+}
+
+async function getCoffeesBetween(minPrice: number, maxPrice: number): Promise<CoffeeModel[]> {
+    const sql = `SELECT * FROM coffees WHERE price BETWEEN ${minPrice} AND ${maxPrice}`;
+    const coffees = await dal.execute(sql);
+    return coffees;
+}
+
+async function getCoffeesByStrength(strength: number): Promise<CoffeeModel[]> {
+    const sql = `SELECT * FROM coffees WHERE strength = ${strength}`;
+    const coffees = await dal.execute(sql);
+    return coffees;
+}
+
+async function addCoffee(coffee: CoffeeModel): Promise<CoffeeModel> {
+    const sql = `INSERT INTO coffees (coffeeType, price, strength) VALUES ('${coffee.coffeeType}', ${coffee.price}, ${coffee.strength})`;
+    const info: OkPacket = await dal.execute(sql);
+    coffee.id = info.insertId;
+    return coffee;
+}
+
+async function updateCoffee(coffee: CoffeeModel): Promise<CoffeeModel> {
+    const sql = `UPDATE coffees SET coffeeType = '${coffee.coffeeType}', price = ${coffee.price}, strength = ${coffee.strength} WHERE coffeeId = ${coffee.id}`;
+    const info: OkPacket = await dal.execute(sql);
+    if (!info.affectedRows) throw new IdNotFound(coffee.id);
+    return coffee;
+}
+
+async function updatePartialCoffee(coffee: CoffeeModel): Promise<CoffeeModel> {
+    const dbCoffee = await getCoffee(coffee.id);
+    for (const prop in coffee) {
+        if (coffee[prop] !== undefined) {
+            dbCoffee[prop] = coffee[prop];
+        }
+    }
+    await updateCoffee(dbCoffee);
+    return dbCoffee;
+}
+
+async function deleteCoffee(id: number): Promise<void> {
+    const sql = `DELETE FROM coffees WHERE coffeeId = ${id}`;
+    const info = await dal.execute(sql);
+    if (!info.affectedRows) throw new IdNotFound(id);
+}
+
+export default {
+    getCoffees,
+    getCoffee,
+    getExpensiveCoffees,
+    getCoffeesBetween,
+    getCoffeesByStrength,
+    addCoffee,
+    updateCoffee,
+    updatePartialCoffee,
+    deleteCoffee
+}
